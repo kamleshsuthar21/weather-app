3b493811eb72957264aa08733990338e21d0b0b8
KAMLESH SUTHAR <kamleshshtharji@gmail.com>
2020-10-15 23:00:15 +0530

    heroku

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,6 @@ const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=25cba980ee62710c979287f8af3fdbc6&query='+longitude+","+latitude
 
     request({ url, json: true }, (error, { body }) => {
-        console.log(body)
         if (error) {
             callback('Unable to connect to weather service!', undefined,undefined,undefined,undefined,undefined,undefined)
         } else if (body.error) {
